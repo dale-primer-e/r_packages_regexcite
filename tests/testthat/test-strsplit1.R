c50985c23a281e097bfd52f3d176acc2be122c94
Dale Euinton <dale@primer-e.co.nz>
2021-11-24 09:21:31 +1300

    Renamed the main function and added more tests

--- tests/testthat/test-strsplit1.R
+++
@@ -1,7 +0,0 @@
-test_that("strsplit1() splits a string", {
-  expect_equal(strsplit1("a,b,c", split = ","), c("a", "b", "c"))
-})
-
-test_that("strsplit1() splits string with different split char", {
-  expect_equal(strsplit1("a-b-c", split = "-"), c("a", "b", "c"))
-})
